4ef938e7b9a928ea15a461280b94e393e0de73a5
Rafael <rcuevae@ulasalle.edu.pe>
2026-07-03 01:48:04 -0500

    Agregar requirements.txt

--- xpdefect_project/settings.py
+++ xpdefect_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yz#uvii_#+i3em+k#7*#2r4qlr1-(8!@cm_li!j40t)8!p3*$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*pip']
 
 
 # Application definition
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
